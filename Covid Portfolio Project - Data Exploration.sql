852d4d330185bce47bb2efd648c2b01273978646
Jad AL Wazzan <67378553+JadAlWazzan@users.noreply.github.com>
2021-07-03 21:25:43 -0400

    Update Covid Portfolio Project - Data Exploration.sql

--- Covid Portfolio Project - Data Exploration.sql
+++ Covid Portfolio Project - Data Exploration.sql
@@ -1,3 +1,9 @@
+/*
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
+
 Select *
 From PortfolioProject..CovidDeath$
 Where continent is not null
